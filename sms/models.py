fb761a4ef6cf5bfb0219ff4b2a117c50a82d73c0
dnyt <lyricaldnyt@gmail.com>
2019-09-24 22:04:18 +0300

    Created first model and registered it in admin file

--- sms/models.py
+++ sms/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+class BulkNumbers(models.Model):
+    name = models.CharField(max_length=50)
+    phone_number = PhoneNumberField()
+    
+    
+    
+
+    class Meta:
+        verbose_name = _("")
+        verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+)

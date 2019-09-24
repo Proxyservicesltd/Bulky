804dfc38fe7176b61abbed81e1d228722a2cf4b1
dnyt <lyricaldnyt@gmail.com>
2019-09-24 22:23:04 +0300

    Ran necessary tests

--- sms/tests.py
+++ sms/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from  .models import BulkNumbers
 
 # Create your tests here.
+class BulkNumberTest(TestCase):
+    def create_number(self):
+        self.assertEqual(str(name),str(phone_number))

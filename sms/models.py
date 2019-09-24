7aa3dcd382967689d913ea609a2a356273589ede
dnyt <lyricaldnyt@gmail.com>
2019-09-24 22:13:24 +0300

    changed model object name

--- sms/models.py
+++ sms/models.py
@@ -5,17 +5,9 @@ from phonenumber_field.modelfields import PhoneNumberField
 class BulkNumbers(models.Model):
     name = models.CharField(max_length=50)
     phone_number = PhoneNumberField()
-    
-    
-    
-
-    class Meta:
-        verbose_name = _("")
-        verbose_name_plural = _("s")
 
     def __str__(self):
         return self.name
+    
 
-    def get_absolute_url(self):
-        return reverse("_detail", kwargs={"pk": self.pk})
-)
+
